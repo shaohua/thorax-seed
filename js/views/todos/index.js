1167aaf731e7803ba2f54d70cd214e5fa6aff10a
Shaohua Zhou <sh.zhou@gmail.com>
2013-07-23 15:21:57 -0700

    Now works

--- js/views/todos/index.js
+++ js/views/todos/index.js
@@ -1,5 +1,15 @@
 Application.View.extend({
-  name: "todos/index"
+  name: "todos/index",
+  events:{
+    "submit form": function (event) {
+      event.preventDefault();
+      var attrs = this.serialize();
+      this.collection.add(attrs);
+      this.$('input[name="title"]').val('');
+      // body...
+    }    
+  }
+
 });
 
 // Instances of this view can be created by calling:
