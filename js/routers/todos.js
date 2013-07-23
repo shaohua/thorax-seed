d44ed3a1c1a256b02c7ef133c2128c21c632b752
Shaohua Zhou <sh.zhou@gmail.com>
2013-07-23 15:10:55 -0700

    Collection works

--- js/routers/todos.js
+++ js/routers/todos.js
@@ -1,8 +1,15 @@
 new (Backbone.Router.extend({
   routes: module.routes,
   index: function () {
+    var collection = new Application.Collection([{
+      title: 'Fish Taco',
+      done: true
+    }])
+
     console.log('routers-todo.js');
-    var view = new Application.Views['todos/index']();
+    var view = new Application.Views['todos/index']({
+      collection: collection
+    });
     Application.setView(view);
   }
 }));
